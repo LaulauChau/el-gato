b64a8f461693d3ba409f0e9d1cd15ffe623ad711
Laurent Chau <laurent.chau@proton.me>
2026-02-03 18:08:49 +0100

    chore: scaffold project with shadcn/create

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
   reactCompiler: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-export default nextConfig;
+export default config;
